6a0712ee4d4fc23baada92c6b3fcd8cf4bbc420a
tufaelkhan <tufaelkhan247@gmail.com>
2023-10-19 09:41:27 -0700

    add style

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ const search = document.querySelector('.search-box button')
 const weatherBox = document.querySelector('.weather-box')
 const weatherDetails = document.querySelector('.weather-details')
 
-search.addEventListener('click', ()=>{
+search.addEventListener('click', () => {
     const APIKey = '145200e04488b30e843840ec465b5d40';
     const city = document.querySelector('.search-box input').value;
 
     if(city == '') return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json =>{
+    const image = document.querySelector('.weather-box img')
+    const temparature = document.querySelector('.weather-box .temparature')
+    const description = document.querySelector('.weather-box .description')
+    const humidiity = document.querySelector('.weather-details .humidiity span')
+    const wind = document.querySelector('.weather-details .wind span')
+
+    switch (json.weather[0].main){
+        case 'Clear':
+            image.src = 'image/clear.avif'
+    }
+    })
 })
